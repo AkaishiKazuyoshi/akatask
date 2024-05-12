cb958320d87941fcafc3da83900c756bccbcff8b
Kazuyoshi Akaishi <k.akaishi@kazuyoshi_akaishi@mail.toyota.co.jp>
2024-05-12 22:29:54 +0900

    [in progress #187565655]User can see login form5/10

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,10 +1,33 @@
-import { expect, it, describe } from 'vitest';
-describe('数値のテスト', () => {
-  it('数値の等価性テスト', () => {
-    expect(2 + 2).toBe(4);
+import { expect, it, describe, vi } from 'vitest';
+import {render, screen} from '@testing-library/react'
+import App from "./App.tsx";
+import axios from "axios";
+import { userEvent } from '@testing-library/user-event'
+
+describe('App.tsx', () => {
+  it('render loginPage', () => {
+    render(<App/>)
+
+    expect(screen.getByText('ID'))
+    expect(screen.getByPlaceholderText('sample@mail.com'))
+    expect(screen.getByText('Password'))
+    expect(screen.getByPlaceholderText('password'))
+    expect(screen.getByRole('button', {
+      name: 'Login'
+    }))
   });
 
-  it('数値の不等価性テスト', () => {
-    expect(2 + 2).not.toBe(5);
+  it('given email, password wrote when click login then request login via axios', async () => {
+    const spyAxiosPost = vi.spyOn(axios, 'post').mockResolvedValue(null)
+    render(<App/>)
+
+    await userEvent.type(screen.getByPlaceholderText('sample@mail.com'), 'akaishi@mail.com')
+    await userEvent.type(screen.getByPlaceholderText('password'), '1234567')
+    await userEvent.click(screen.getByRole('button', { name: 'Login' }))
+
+    expect(spyAxiosPost).toHaveBeenCalledWith('http://localhost:8080/api/login', {
+      id: 'akaishi@mail.com',
+      password: '1234567'
+    })
   })
 })
