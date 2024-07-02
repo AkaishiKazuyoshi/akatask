8cfb36dc1086fc984e08a82796cd496cd87b4bdc
Kazuyoshi Akaishi <k.akaishi@kazuyoshi_akaishi@mail.toyota.co.jp>
2024-07-03 07:52:58 +0900

    [login service] make encryption using longin

--- backend/src/main/kotlin/com/akaishi/task/service/LoginService.kt
+++ backend/src/main/kotlin/com/akaishi/task/service/LoginService.kt
@@ -1,7 +1,13 @@
 package com.akaishi.task.service
 
 import com.akaishi.task.repository.LoginRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import java.security.InvalidKeyException
+import java.security.NoSuchAlgorithmException
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
 
 interface LoginService {
     fun login(id: String, password: String): Boolean
@@ -9,11 +15,32 @@ interface LoginService {
 
 @Service
 class DefaultLoginService(
+    @Value("aka-task.secret")
+    private val secret: String,
     private val loginRepository: LoginRepository
 ) : LoginService {
+
+    private fun bytesToHexString(bytes: ByteArray): String {
+        val hash = StringBuilder()
+        for (aByte in bytes) {
+            val hex = "%02x".format(aByte)
+            hash.append(hex)
+        }
+        return hash.toString()
+    }
+
+    @Throws(NoSuchAlgorithmException::class, InvalidKeyException::class)
+    fun hmacWith(data: String): String {
+        val secretKeySpec = SecretKeySpec(secret.toByteArray(), "HmacSHA256")
+        val mac = Mac.getInstance("HmacSHA256") // MAC(keyed-hash message authentication code) -> encrypted code
+        mac.init(secretKeySpec)
+        return bytesToHexString(mac.doFinal(data.toByteArray()))
+    }
+
     // Encryption concept
     override fun login(id: String, password: String): Boolean {
         val optionalUser = loginRepository.findById(id)
-        return optionalUser.isPresent && optionalUser.get().password == password
+        val encryptedPassword = hmacWith(password)
+        return optionalUser.isPresent && optionalUser.get().password == encryptedPassword
     }
 }
--- backend/src/test/kotlin/com/akaishi/task/service/LoginServiceTest.kt
+++ backend/src/test/kotlin/com/akaishi/task/service/LoginServiceTest.kt
@@ -17,11 +17,16 @@ class LoginServiceTest {
 
     @Test
     fun `given collect id and password when login was called return true`() {
-        loginRepository.save(UserEntity(id = "1234", password = "abcd"))
+        loginRepository.save(
+            UserEntity(
+                id = "1234",
+                password = "0583100aa8ee5fb10f459eefd9693ea8b8b9ca5d26ccadbd85d1508ba10bc6f1"
+            )
+        )
 
 
-        sut = DefaultLoginService(loginRepository = loginRepository)
-        val result = sut.login(id = "1234", password = "abcd")
+        sut = DefaultLoginService(secret = "aka-task-key", loginRepository = loginRepository)
+        val result = sut.login(id = "1234", password = "12345")
 
 
         assertTrue(result)
@@ -29,7 +34,7 @@ class LoginServiceTest {
 
     @Test
     fun `given user is not existed when login was called return false`() {
-        sut = DefaultLoginService(loginRepository = loginRepository)
+        sut = DefaultLoginService(secret = "aka-task-key", loginRepository = loginRepository)
         val result = sut.login(id = "1234", password = "abcd")
 
 
@@ -39,10 +44,15 @@ class LoginServiceTest {
 
     @Test
     fun `given invalid password existed when login was called return false`() {
-        loginRepository.save(UserEntity(id = "1234", password = "abcd"))
+        loginRepository.save(
+            UserEntity(
+                id = "1234",
+                password = "0583100aa8ee5fb10f459eefd9693ea8b8b9ca5d26ccadbd85d1508ba10bc6f1"
+            )
+        )
 
 
-        sut = DefaultLoginService(loginRepository = loginRepository)
+        sut = DefaultLoginService(secret = "aka-task-key", loginRepository = loginRepository)
         val result = sut.login(id = "1234", password = "efgh")
 
 
