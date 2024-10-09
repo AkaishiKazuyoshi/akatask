e5a8684936f581c62c4536c9fafc8799c11ebcda
Kazuyoshi Akaishi <k.akaishi@kazuyoshi_akaishi@mail.toyota.co.jp>
2024-10-09 10:35:29 +0900

    dynamodb backend setup

---
+++ backend/src/main/kotlin/com/akaishi/task/config/DynamoDbConfig.kt
@@ -0,0 +1,34 @@
+package com.akaishi.task.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import java.net.URI
+
+@Configuration
+class DynamoDbConfig(
+    @Value("\${amazon.dynamodb.endpoint}") private val endpoint: String,
+) {
+
+    @Bean
+    fun dynamoDbClient(): DynamoDbClient {
+        val builder = DynamoDbClient.builder().region(Region.AP_NORTHEAST_1)
+        if (endpoint != "default") {
+            builder.endpointOverride(URI.create(endpoint)).credentialsProvider(
+                    StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create("dummy", "dummy"),
+                    ),
+                )
+        }
+        return builder.build()
+    }
+
+    @Bean
+    fun dynamoDbEnhancedClient(dynamoDbClient: DynamoDbClient): DynamoDbEnhancedClient =
+        DynamoDbEnhancedClient.builder().dynamoDbClient(dynamoDbClient).build()
+}
